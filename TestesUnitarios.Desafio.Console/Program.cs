ed458c3df38d19578481b7752ea6448b151dfc9a
Gabriel Barros <gabriellourencobarros02@gmail.com>
2023-09-27 22:24:14 -0300

    Adicionando alterações do fork

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TestesUnitarios.Desafio.Console.Services;
+
 Console.WriteLine("Hello, World!");
+var valida = new ValidacoesLista();
+Console.WriteLine(valida.MultiplicarNumerosLista(new List<int> { 5, 7, 8, 9 }, 2));
